18fc881cd2cd7192dcfdbb9f46dd0cf877815a2d
nosianuri <nurinosia@gmail.com>
2022-09-11 16:37:20 +0600

    set banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles";
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 function App() {
   return (
+    <>
+    <Particles
+    className='particles-canvas'
+    params={{
+      particles: {
+        number: {
+          value: 30,
+          density: {
+            enable: true,
+            value_area: 900,
+          }
+        },
+        shape: {
+          type: "square",
+          stroke: {
+            width: 6,
+            color: "#f9ab00"
+          }
+        }
+      },
+    }}
+     />
     <Navbar />
+    <Header />
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import logo from "../logo.png";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#"><img className='logo' src={logo} alt="logo..." /></a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <FontAwesomeIcon icon={faBars} style={{color: '#fff'}} />
             </button>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mx-auto ">
                         <li className="nav-item active">
 
