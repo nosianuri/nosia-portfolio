89ff37e54d5927d8b298e6a897be4e54a92d1f15
nosianuri <nurinosia@gmail.com>
2022-10-08 14:23:02 +0600

    overall setup

--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
+import React from 'react';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import Particles from "react-particles";
-import Navbar from './components/Navbar';
-import Header from './components/Header';
-import { Skills } from './components/Skills';
-import { Projects } from './components/Project';
-import Contact from './components/Contact';
-import { Footer } from './components/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
-    <Particles
-    className='particles-canvas'
-    params={{
-      particles: {
-        number: {
-          value: 30,
-          density: {
-            enable: true,
-            value_area: 900,
-          }
-        },
-        shape: {
-          type: "square",
-          stroke: {
-            width: 6,
-            color: "#f9ab00"
-          }
-        }
-      },
-    }}
-     />
-    <Navbar />
-    <Header />
-    <Skills />
-    <Projects />
-    <Contact />
-    <Footer />
+    <Home />
     </>
   );
 }
