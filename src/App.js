000384f037de45d7a274fef588adf97e4bf162d0
nosianuri <nurinosia@gmail.com>
2022-10-19 18:38:15 +0600

    added contact page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Contacts from './components/Contacts';
 import Home from './components/Home';
 import Portfolio from './components/Portfolio';
 import Resume from './components/Resume';
@@ -18,6 +19,7 @@ function App() {
       <Route exact path='/' element={<Home />} />
       <Route path='/resume' element={<Resume />} />
       <Route path='/portfolio' element={<Portfolio />} />
+      <Route path='/contacts' element={<Contacts />} />
       </Routes>
     </>
   );
